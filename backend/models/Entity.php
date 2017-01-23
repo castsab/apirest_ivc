c7955755b33fe11383645236d01547300dd79638
Andres Castellanos <castellanos1458@gmail.com>
2017-01-23 15:42:56 -0500

    Added rest controller for Entity and Visit models

---
+++ backend/models/Entity.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "entity".
+ *
+ * @property integer $id
+ * @property string $name_entity
+ *
+ * @property Visit[] $visits
+ */
+class Entity extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'entity';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'name_entity'], 'required'],
+            [['id'], 'integer'],
+            [['name_entity'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name_entity' => Yii::t('app', 'Name Entity'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getVisits()
+    {
+        return $this->hasMany(Visit::className(), ['id_entity' => 'id']);
+    }
+}
